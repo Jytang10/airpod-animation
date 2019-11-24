faf2847f23228fe33d5a6789d28ca48eda15799c
James Tang <jytang10@gmail.com>
2019-11-23 22:25:40 -0800

    Added controller

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const text = intro.querySelector('h1');
 //END SECTIOn
 const section = document.querySelector('section');
 const endText = section.querySelector('h1');
+
+//SCROLL MAGIC
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  duration: 9000,       //Change based on content and need. Video duration is 9 seconds in this project
+  triggerElement: intro,
+  triggerHook: 0        //Position to finish some action (unpin, etc.), or start animation (trigger effect)
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
