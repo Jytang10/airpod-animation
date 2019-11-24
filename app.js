27888bc86dfa7bcd2b1fecd9a1e0d02b50986bf4
James Tang <jytang10@gmail.com>
2019-11-23 22:15:28 -0800

    Created app.js with query selectors

---
+++ app.js
@@ -0,0 +1,6 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+//END SECTIOn
+const section = document.querySelector('section');
+const endText = section.querySelector('h1');
